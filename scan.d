897c7804cf6d0e5e6ee26e37dcc01a1c70d74d25
Matt Kline <matt@bitbashing.io>
2015-07-06 18:43:01 -0700

    Add --help to scanner

--- scan.d
+++ scan.d
@@ -22,14 +22,13 @@ int main(string[] args)
             config.caseSensitive,
             config.bundling,
             "help|h", { writeln(helpText); exit(0); },
-            "version|v", { writeln(versionText); exit(0); },
-            "machine|m", &machine,
-            "recursive|r", &recursive
-            );
+            "version", { writeln(versionText); exit(0); },
+            "recursive|r", &recursive,
+            "machine|m", &machine);
     }
     catch (GetOptException e) {
         stderr.writeln(e.msg);
-        stderr.writeln("See scan --help for more information.");
+        stderr.writeln("See --help for more information.");
         exit(1);
     }
 
@@ -55,6 +54,8 @@ int main(string[] args)
 
 size_t pessimalSize(const ref FileInfo fi)
 {
+    // The largest the file could be is the max of its actual size or,
+    // if the file is sparse, its logical size rounded up to the nearest block.
     return max(fi.logicalSize + (fi.blockSize - fi.logicalSize % fi.blockSize),
                fi.actualSize);
 }
@@ -62,14 +63,49 @@ size_t pessimalSize(const ref FileInfo fi)
 size_t possibleSavings(const ref FileInfo fi, size_t zeroSpace)
 {
     immutable pessimal = pessimalSize(fi);
-    immutable optimal = pessimal - zeroSpace;
+    immutable optimal = pessimal - zeroSpace; // The smallest it could be
+
+    // The amount of space we can save is the difference between the optimal
+    // size and the current (actual) size, provided that value is positive.
     return fi.actualSize <= optimal ? 0 : fi.actualSize - optimal;
 }
 
 string helpText = q"EOS
+Usage: holescan [<options>] <files and directories>
+
+Scans files for space that could be saved as holes.
+
+Several Linux filesystems (XFS, ext4, btrfs, tmpfs) support sparse files, i.e.
+files that save space by omitting empty filesystem blocks that contain only
+zeroes. Unfortunately, these "holes" in the files aren't automatically created
+by writing a string of zeroes, but only by seeking past them with `fseek`,
+`lseek`, etc. It's entirely possible that more room on your hard drive could be
+saved by finding empty blocks and replacing them with holes.
+
+This utility scans through the given files and reports on how much space can be
+saved for each. It currently assumes that it is on a filesystem that supports
+sparse files. Functionality to check this beforehand may be added in future
+versions.
+
+Options:
+
+  --help, -h
+    Show this text and exit.
+
+  --version
+    Show version information and exit.
+
+  --recursive
+    Recursively search specified directories, which are otherwise ignored.
+
+  --machine, -m
+    Display output better suited for by other scripts/programs instead of
+    humans. When specified, each line of output will consist of the file path, a
+    space, then the number of bytes that could be saved by punching holes.
 EOS";
 
 string versionText = q"EOS
-filepunch, v 0.1
-by Matt Kline, 2015
+holescan, v 0.1
+Part of the filepunch toolset by Matt Kline, 2015
+https://github.com/mrkline/filepunch
 EOS";
